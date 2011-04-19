518a5929d1a719649ef5de09f6c10d502db8d138
Graham Christensen <gchristensen@nationalfield.org>
2011-04-19 17:31:16 -0400

    Initial import.

---
+++ PHPUnit/Extensions/SeleniumPageObject.php
@@ -0,0 +1,106 @@
+<?php
+
+abstract class PHPUnit_Extensions_SeleniumPageObject
+{
+
+    /**
+     * The testcase that is utilizing the page object.
+     *
+     * @var PHPUnit_Extensions_SeleniumTestCase
+     */
+    protected $se;
+    /**
+     * A mapping of unique keys to locator strings. Each one of these is
+     * validated to ensure it exists on the page when the PageObject is
+     * instantiated.
+     *
+     * @var array
+     */
+    protected $map = array();
+
+    /**
+     * Instantiate the PageObject and validate that the browser in a valid
+     * state for this PageObject.
+     *
+     * @param PHPUnit_Extensions_SeleniumTestCase $driver
+     */
+    public function __construct(PHPUnit_Extensions_SeleniumTestCase $test)
+    {
+        $this->se = $test;
+
+        $this->assertMapConditions();
+        $this->assertPreConditions();
+    }
+
+    /**
+     * Assert that all elements in $this->map are present on the page
+     */
+    protected function assertMapConditions()
+    {
+        foreach ($this->map as $field => $locator) {
+            $this->se->assertTrue(
+                $this->se->isElementPresent($locator),
+                'Locator field "' . $field . '" is not present.');
+        }
+    }
+
+    /**
+     * You may want to assert a page's header or title. If all you are testing
+     * is that a field is present on the page, add the locator to the $map.
+     */
+    protected function assertPreConditions()
+    {
+        // Placeholder
+    }
+
+    /**
+     * Convert a *ByMap call to using the real locator string as stored in
+     * $this->map
+     *
+     * @example
+     * // In the class definition
+     * protected $map = array('title' => '//div[@id="title"]')
+     *
+     * // In your PageObject
+     * public function getTitle()
+     * {
+     *     return $this->getTextByMap('title');
+     * }
+     *
+     * @param string $name Method name
+     * @param array $arguments An array of arguments. The first one must be the
+     * map locator.
+     *
+     * @return mixed
+     */
+    public function __call($name, $arguments)
+    {
+        // Only intercept *ByMap calls
+        if (substr($name, -5) == 'ByMap') {
+            // trim off the ByMap
+            $name = substr($name, 0, -5);
+
+            // Replace the map key with the locator stored in $this->map
+            $arguments[0] = $this->getLocator($arguments[0]);
+        }
+
+        return call_user_func_array(array($this->se, $name), $arguments);
+    }
+
+    /**
+     * Get a map key's locator string
+     *
+     * @param string $map
+     * @return string
+     * @throws InvalidArgumentException If the $map does not exist.
+     */
+    protected function getLocator($map)
+    {
+        if (isset($this->map[$map])) {
+            return $this->map[$map];
+        }
+
+        throw new InvalidArgumentException('Map ' . $map . ' is not a valid locator key.');
+    }
+
+}
---
+++ PHPUnit/Extensions/SeleniumPageObject/Model.php
@@ -0,0 +1,116 @@
+<?php
+
+abstract class PHPUnit_SeleniumPageObject_Model extends NF_Selenium_SeleniumPageObject
+{
+    /**
+     * An array of mappings that do not directly translate to the model with
+     * getter and setters. These should be implemented manually in setFromModel
+     * and assertEqualsModel
+     * 
+     * @var array
+     */
+    protected $modelSkip = array();
+
+    /**
+     * Import a model's data into the PageObject.
+     *
+     * Uses $object->getCamelMapKey() and $this->setCamelMapKey() to make the
+     * translation. If specific map keys do not match this get/set method, list
+     * them in $this->modelSkip and make sure to implement
+     * the setting in your class's setFromModel() method.
+     *
+     * 
+     * @param object $object The object you want to pass
+     * @return self
+     */
+    public function setFromModel($object)
+    {
+        foreach ($this->map as $field => $locator) {
+            if (!in_array($field, $this->modelSkip)) {
+                self::setByField($this, $field, self::getByField($object, $field));
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * Test to ensure that the current PageObject represents a model's values
+     *
+     * Uses $object->getCamelMapKey() and $this->setCamelMapKey() to make the
+     * translation. If specific map keys do not match this get/set pattern, list
+     * them in $this->modelSkip and make sure to implement
+     * the comparison in your class's assertEqualsModel() method.
+     * 
+     *
+     * @param object $object
+     * @param string $message
+     */
+    public function assertEqualsModel($object, $message = '')
+    {
+        foreach ($this->map as $field => $locator) {
+            if (!in_array($field, $this->modelSkip)) {
+                // Get the value from the PageObject
+                $value = self::getByField($this, $field);
+
+                // Get the value from the model $object
+                $expect = self::getByField($object, $field);
+
+                // If what is passed is an object, convert to a string using
+                // __toString
+                if (is_object($expect)) {
+                    $expect = (string) $expect;
+                }
+
+                $this->se->assertEquals($expect, $value. $message . ' (' . $field . ')');
+            }
+        }
+    }
+
+    /**
+     * Using a getter in the pattern of getCamelCase, convert the $field name
+     * to camel case and get the value from $object.
+     *
+     * @example For getByField($obj, 'user_count') this will call
+     * $obj->getUserCount()
+     *
+     * @param object $object
+     * @param string $field
+     * @return mixed
+     */
+    protected static function getByField($object, $field)
+    {
+        $get = 'get' . self::getCamelField($field);
+        return $object->$get();
+    }
+
+    /**
+     * Using a setter in the pattern of getCamelCase, convert the $field name
+     * to camel case and set the value from $object.
+     *
+     * @example For setByField($obj, 'user_count', 123) this will call
+     * $obj->setUserCount(123)
+     *
+     * @param object $object
+     * @param string $field
+     * @param mixed $value
+     * @return mixed
+     */
+    protected static function setByField($object, $field, $value)
+    {
+        $set = 'set' . self::getCamelField($field);
+        return $object->$set($value);
+    }
+
+    /**
+     * Convert a field name to camel case for usage in the getter and setter.
+     *
+     * @param string $field
+     * @return string
+     */
+    protected static function getCamelField($field)
+    {
+        return str_replace(' ', '', ucwords(str_replace('_', ' ', $field)));
+    }
+
+}
